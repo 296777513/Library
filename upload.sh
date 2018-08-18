e290ef829464b473563f1a1d325942c9631aaad3
liyachao <liyachao.knight@bytedance.com>
2018-08-18 23:45:57 +0800

    feature:Structure recreate

--- upload.sh
+++ upload.sh
@@ -11,12 +11,17 @@ echo ${str}
 sed -i "" "s/${origin}/${str}/g" gradle.properties
 #./gradlew clean build bintrayUpload -PbintrayUser=296777513 -PbintrayKey=0e35550c9145602f043f4cb98f2a12b9a6bbb98f -PdryRun=false
 echo "begin upload please wait!!!~~~"
+
 uploadStr=`./gradlew clean build bintrayUpload -PbintrayUser=296777513 -PbintrayKey=0e35550c9145602f043f4cb98f2a12b9a6bbb98f -PdryRun=false 2>&1`
+
 echo ${uploadStr}
 
 if [[ ${uploadStr} =~ "BUILD SUCCESSFUL" ]]
 then
-    echo "UPLOAD SUCCESS "
+    echo "========UPLOAD SUCCESS "
+    git status
+    git add ./gradle.properties
+    git commit -m "feature: upload version code $str"
 else
     echo "SUCCESS~~~~"
     sed -i "" "s/${str}/${origin}/g" gradle.properties
